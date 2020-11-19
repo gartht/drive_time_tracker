973119f98cdba10e315cd21ee89403bafdf2e605
Garth <garth.tutor@assettrack.onmicrosoft.com>
2020-11-19 10:47:07 -0800

    Changed button from non-descript flat button to more obvious colored raised button

--- lib/screens/HomeScreen.dart
+++ lib/screens/HomeScreen.dart
@@ -11,6 +11,11 @@ class HomeScreen extends StatefulWidget {
   _HomeScreenState createState() => _HomeScreenState();
 }
 
+const Color drivingColor = Colors.red;
+const Color drivingAccent = Colors.redAccent;
+const Color idleColor = Colors.green;
+const Color idleAccent = Colors.greenAccent;
+
 class _HomeScreenState extends State<HomeScreen> {
   final String notRunningText = "Start tracking your time.";
   final String runningText = "Finish tracking your time.";
@@ -28,28 +33,53 @@ class _HomeScreenState extends State<HomeScreen> {
 
   List<Widget> get _layout {
     var widgets = List<Widget>();
-    if (running) {
-      widgets.add(Text("Tracking Drive Time",
-          style: TextStyle(
-              color: Colors.green[500],
-              fontSize: 20,
-              fontWeight: FontWeight.bold)));
-    }
 
-    widgets.add(Center(
-      child: FlatButton(
-        child: Text(running ? runningText : notRunningText),
-        onPressed: () {
-          setState(() {
-            if (running) {
-              _driveEnd = DateTime.now();
-              _saveDrive();
-            } else {
-              _driveStart = DateTime.now();
-            }
-            running = !running;
-          });
-        },
+    widgets.add(
+      Text(
+        running ? "Tracking Drive Time" : "",
+        style: TextStyle(
+            color: Colors.green[500],
+            fontSize: 30,
+            fontWeight: FontWeight.bold),
+        textAlign: TextAlign.center,
+      ),
+    );
+
+    widgets.add(Expanded(
+      child: Center(
+        child: RaisedButton(
+          elevation: 12.0,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(60.0),
+            side: BorderSide(
+              color: running ? drivingAccent : idleAccent,
+              width: 2,
+            ),
+          ),
+          color: running ? drivingColor : idleColor,
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(
+              running ? runningText : notRunningText,
+              style: TextStyle(
+                fontSize: 35.0,
+                color: Colors.white,
+              ),
+              textAlign: TextAlign.center,
+            ),
+          ),
+          onPressed: () {
+            setState(() {
+              if (running) {
+                _driveEnd = DateTime.now();
+                _saveDrive();
+              } else {
+                _driveStart = DateTime.now();
+              }
+              running = !running;
+            });
+          },
+        ),
       ),
     ));
 
@@ -67,15 +97,19 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
-        actions: [IconButton(icon: Icon(Icons.list), onPressed: _buttonAction)],
-      ),
-      body: Column(children: [
-        SizedBox(
-          height: 20.0,
+        title: Text(
+          widget.title,
         ),
-        ..._layout,
-      ]),
+        actions: [IconButton(icon: Icon(Icons.list), onPressed: _buttonAction)],
+        centerTitle: true,
+      ),
+      body: Column(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            ..._layout,
+          ]),
     );
   }
 }
